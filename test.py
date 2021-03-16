52abd64b51f163b36d2e691b9c48505630c0ee19
Oteki <oteki@mbe.rcast.u-tokyo.ac.jp>
2021-03-16 16:06:09 +0900

    mode dispersion

--- test.py
+++ test.py
@@ -4,15 +4,14 @@ import matplotlib.pyplot as plt
 import n
 import draw
 
-wl0 = np.arange(1200, 1200+500, 500)*1e-9    #1200e-9
+wl = 1200e-9
+m0 = np.arange(0, 6)
 L0 = np.arange(10, 2000+10, 10)*1e-9
-wl = np.tile(wl0, (len(L0), 1))
-L = np.tile(L0, (len(wl0), 1)).transpose()
-Nt = n.AlGaAs(0.8, wl)
-Ng = n.AlGaAs(0.0, wl)
-Ns = n.AlGaAs(0.8, wl)
-m = 0
-
+L = np.tile(L0, (len(m0), 1)).transpose()
+m = np.tile(m0, (len(L0), 1))
+Nt = np.tile(n.AlGaAs(0.8, wl), (len(L0), len(m0)))
+Ng = np.tile(n.AlGaAs(0.0, wl), (len(L0), len(m0)))
+Ns = np.tile(n.AlGaAs(0.8, wl), (len(L0), len(m0)))
 
 def left(x):
     return 2*pi*L/wl*sqrt(Ng**2-x**2)
@@ -39,7 +38,9 @@ def BinarySearch(eq, upper,lower):
 Ne = BinarySearch(h, Ng, Ns)
 
 k0 = 2*pi/wl
-ky, As, At = k0*[sqrt(Ng**2-Ne**2), sqrt(Ne**2-Ns**2), sqrt(Ne**2-Nt**2)]
+ky = k0*sqrt(Ng**2-Ne**2)
+As = k0*sqrt(Ne**2-Ns**2)
+At = k0*sqrt(Ne**2-Nt**2)
 phi = arctan(-As/ky)
 
 E1, E2, E3 = [1/(2*As), L/2+sin(2*ky*L)/(4*ky), (cos(ky*L))**2/(2*At)]
@@ -48,24 +49,24 @@ Gamma[Ng-Ne<1e-5] = 0.0
 b = (Ne**2-Nt**2)/(Ng**2-Nt**2)
 b[Ng-Ne<1e-5] = 0.0
 
-draw.graph1(L0, wl0, b, Gamma)
+draw.graph1(L0, m0, b, Gamma)
 
 
 def f(yi, j, k):
     if yi<0:
         return cos(phi[j][k])*exp(As[j][k]*yi)
     elif yi<L[j][k]:
-        return cos(k0[j][k]*sqrt(Ng[j][k]**2-Ne[j][k]**2)*yi+phi[j][k])
+        return cos(k0*sqrt(Ng[j][k]**2-Ne[j][k]**2)*yi+phi[j][k])
     else:
-        return cos(k0[j][k]*sqrt(Ng[j][k]**2-Ne[j][k]**2)*L[j][k]+phi[j][k])*exp(-At[j][k]*(yi-L[j][k]))
+        return cos(k0*sqrt(Ng[j][k]**2-Ne[j][k]**2)*L[j][k]+phi[j][k])*exp(-At[j][k]*(yi-L[j][k]))
 
 div = 1000
 y = np.empty((len(L0), div))
-E = np.empty((len(L0), len(wl0), div))
+E = np.empty((len(L0), len(m0), div))
 for j, Lj in enumerate(L0):
-    for k, wlk in enumerate(wl0):
+    for k, wlk in enumerate(m0):
         y[j] = np.linspace(-L0[j]*3/2, L0[j]*3/2, div)
         E[j][k] = np.array([f(yi+L0[j]/2, j, k) for yi in y[j]])
 
 j = -1
-draw.graph2(L0, wl0, j, E, y)
+draw.graph2(L0, m0, j, E, y)
